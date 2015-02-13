3b6944167368866b597bcc4628d82ddc5d2a085c
daesun <yume@yumes.net>
2015-02-13 15:41:49 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Files service
+=====
+[![Build Status](https://travis-ci.org/lecle/filesservice.svg?branch=master)](https://travis-ci.org/lecle/filesservice)
+
+Files service
---
+++ test/files-test.js
@@ -0,0 +1,49 @@
+var files = require('../lib/files');
+
+var req = {
+    data : {
+
+        checklist : ['APIAUTH', 'SESSION', 'MASTERKEY'],
+        headers : {
+            'x-noserv-session-token' : 'supertoken',
+            'x-noserv-application-id' : 'supertoken',
+            'x-noserv-master-key' : 'supertoken'
+        }
+    }
+};
+
+var res = function(done) {
+
+    return {
+        send : function() {done();},
+        error : function(err) {done(err);}
+    };
+};
+
+var dummyContainer = {
+    addListener : function(){},
+    getService : function(name) {
+
+        return {
+            then : function(callback){ callback({send : function(command, data, callback) {
+
+                callback(null, {data : {masterKey : 'test'}});
+            }});
+
+                return {fail : function(){}};
+            }
+        };
+    }
+};
+
+describe('files', function() {
+    describe('#init()', function () {
+        it('should initialize without error', function (done) {
+
+            files.init(dummyContainer, function (err) {
+
+                files.close(done);
+            });
+        });
+    });
+});
